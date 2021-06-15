20b103efad59d831af3307a971a5ba245b059535
Tony Maddalone <tjmaddalone@gmail.com>
2021-06-15 17:21:26 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,9 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_logo = "_static/logo.png"
+
+html_css_files = ["css/toggle.css"]
+
+html_js_files = ["js/matomo.js", "js/toggle.js"]
+
+
